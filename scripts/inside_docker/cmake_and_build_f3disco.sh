453a4974f86320d4c2436de43ef704999dcbaac9
Max Dignan <maxdignan@gmail.com>
2022-02-11 21:33:46 -0500

    Updated cmakelists and some dependencies inside of lvgl to be able to compile lvgl to embedded. Unfortunately f3disco doesnt have enough RAM. Changes to using Ninja instead of make - much faster. Cleaned up some of the Cmakelist to make it compile for both targets. Included a views section inside the PROJECT that will be where we write view code for the menus

--- scripts/inside_docker/cmake_and_build_f3disco.sh
+++ scripts/inside_docker/cmake_and_build_f3disco.sh
@@ -1,5 +1,5 @@
 echo "hi"
-mkdir -p build/DebugF3Disco
-cd build/DebugF3Disco
-cmake ../.. -DCMAKE_TOOLCHAIN_FILE=../../toolchains/gcc-arm-none-eabi.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=true
-make
+mkdir -p build-ninja/DebugF3Disco
+cd build-ninja/DebugF3Disco
+cmake ../.. -GNinja -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_TOOLCHAIN_FILE=../../toolchains/gcc-arm-none-eabi.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=true
+cmake --build .
---
+++ src/PROJECT/views/main_menu.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
+
+void mmbtc_main_menu_render(void);
+
+#endif // MAIN_MENU_H
